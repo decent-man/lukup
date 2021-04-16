975841b765a3b3f55cebac6df9c5fe145868d807
decent-man <decentboi6969@gmail.com>
2021-04-16 10:16:12 +0530

    Updated Instructions

--- src/test.py
+++ src/test.py
@@ -1,4 +1,14 @@
-from master import dhe_scraper
-import sys
+from master import dhe_scraper # <= import like this
+import sys                     # <= needed to read arguments
 print(dhe_scraper(sys.argv[1]))
+#                      /\
+#                      ||
+#           It takes file name as input
+
 # Run this file as:-  python test.py profiles/<<PROFILE_FILE>>.json <<Search_Keyword_Here>>
+#                                             /\                            /\
+#                                             ||                            ||
+#                                   Input the json file as the       Input the search term as the 
+#                                   first argument.                  second argument.
+#                                   All json files are stored
+#                                   in "profiles" directory.
