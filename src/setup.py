47a6bd7ae74225ccab2396458fcfdf42a4a0e852
decent-man <decentboi6969@gmail.com>
2021-04-18 20:10:19 +0530

    Successfully implemented webinspector opening,link analyzer & json creator

--- src/setup.py
+++ src/setup.py
@@ -3,11 +3,14 @@ import sys
 import time
 import pyautogui
 from selenium import webdriver
+import json
+from dataclasses import dataclass, asdict
 # irrelevent modules - for now
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.action_chains import ActionChains
 
+#=========VAR's============
 FIRE_PATH='/home/rakshit/Downloads/geckodriver'
 GIBRL=sys.argv[1]
 GIBQUERY=sys.argv[2]
@@ -18,39 +21,75 @@ DEFAULT_SEP="+"
 
 VALID_QUERY=sys.argv[5:]
 
-DEFAULT_UA = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 RuxitSynthetic/1.0 v9139690441 t38550 ath9b965f92 altpub cvcv=2",
-DEFAULT_REFERER = "https://duckduckgo.com?q=wiki",
-TAG = ""
+DEFAULT_UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 RuxitSynthetic/1.0 v9139690441 t38550 ath9b965f92 altpub cvcv=2'
+DEFAULT_REFERER = 'https://duckduckgo.com?q=wiki'
+SAMPLE_XPATH = '//*[@id="content"]'
+TAG=""
+ATTRN=""
+ATTRTY=""
+SAMPLE_FILENAME="ArchWiki"
+#==========================
 
-def browInspector():
+#=====DATACLASSES==========
+@dataclass
+class PROFILE:
+    AGENT: str
+    REFERER: str
+    URL: str
+    SEP: str
+    TAG_TYPE: str
+    ATTR_NAM: str
+    ATTR_VAL: str
+
+@dataclass
+class XPROFILE:
+    AGENT: str
+    REFERER: str
+    URL: str
+    SEP: str
+    XPATH: str
+#=========================
+
+#=====CORE_FUNCTION========
+def coreSetup():
    ##Search for a GIBBERISH query user!
-   gib1URL = generate_static(GIBRL,GIBQUERY) 
+   gib1URL = Gen_STATIC(GIBRL,GIBQUERY) 
    ##Search for a GIBBERISH query again user!
-   gib2URL = generate_static(GIB2RL,GIB2QUERY)
+   gib2URL = Gen_STATIC(GIB2RL,GIB2QUERY)
    print("\nGibberish1 URL:" + GIBRL + "\nGibberish Parsed:" + gib1URL + "\n\nGibberish2 URL:" + GIB2RL + "\nGibberish2 Parsed:" + gib2URL + "\n" )
 
    if gib2URL == gib1URL:
        browser = webdriver.Firefox(executable_path=FIRE_PATH)
-       browser.get(generate_url(gib1URL,VALID_QUERY))
-       # print(generate_url(gib1URL,VALID_QUERY))
+       browser.get(Gen_URL(gib1URL,VALID_QUERY))
+       # print(Gen_URL(gib1URL,VALID_QUERY))
        #ask if opened page matches the expected page.
        # browser.find_element_by_tag_name('body').send_keys(Keys.ALT + Keys.SHIFT + 'f') # this works for some reason - only strokes sent to the body work
        pyautogui.press('f12')
        pyautogui.hotkey('ctrl','shift','c')
        #CALL WINDOW TO FEED THE TAG
-       time.sleep(15)
+       # Gen_JSON(PROFILE(,,,,),filename) || Gen_JSON(XPROFILE(,,,,),filename)
+       # time.sleep(15)
        browser.quit()
-
+       Write_JSON(Gen_JSON(XPROFILE(DEFAULT_UA,DEFAULT_REFERER,gib2URL,DEFAULT_SEP,SAMPLE_XPATH)),SAMPLE_FILENAME)
+       
    else:
       #PARSE_MISMATCH - do some extra hoops
       print("REGEX_FAILURE:PARSE:1")
+#==========================
 
-
-def generate_static(rawlink,query):
+#=====GENERATORS===========
+def Gen_STATIC(rawlink,query):
    query_pos = rawlink.lower().find(query.lower())
    return str(rawlink[0:query_pos]) # Cuts the string to the point until the searched query word is found.
 
-def generate_url(static,query):
-   return static + DEFAULT_SEP.join(str(word) for word in query) #combines the static_url with the valid search query and replaces spaces with '+'
+def Gen_URL(static,query):
+   return static + DEFAULT_SEP.join(str(word) for word in query) #combines the static_URL with the valid search query and replaces spaces with '+'
 
-browInspector()
+def Gen_JSON(profileobj):
+    return json.dumps(asdict(profileobj),sort_keys=True,indent=0)
+
+def Write_JSON(jsonobj,siteName):
+    open(siteName,'x').write(jsonobj)
+#==========================
+
+coreSetup()
