d13a1123c5ddbef0779b2347206dfd9e3a5579be
decent-man <decentboi6969@gmail.com>
2021-04-19 20:16:35 +0530

    Setup is now partially capable of modular usage

--- src/setup.py
+++ src/setup.py
@@ -27,7 +27,7 @@ ATTRTY=""
 SAMPLE_FILENAME="ArchWiki"
 #==========================
 
-#=====DATACLASSES==========
+#=====PROFILE_DATACLASS==========
 @dataclass
 class PROFILE:
     AGENT: str
@@ -37,37 +37,22 @@ class PROFILE:
     TAG_TYPE: str
     ATTR_ID: str
     ATTR_VAL: str
-
-# @dataclass
-# class XPROFILE:
-    # AGENT: str
-    # REFERER: str
-    # URL: str
-    # SEP: str
-    # XPATH: str
 #=========================
 
 #=====CORE_FUNCTION========
 def coreSetup():
-   #Search for a GIBBERISH query user!
-   gib1URL = Gen_STATIC(GIBRL,GIBQUERY) 
-   #Search for a GIBBERISH query again user!
-   gib2URL = Gen_STATIC(GIB2RL,GIB2QUERY)
-   # print("\nGibberish1 URL:" + GIBRL + "\nGibberish Parsed:" + gib1URL + "\n\nGibberish2 URL:" + GIB2RL + "\nGibberish2 Parsed:" + gib2URL + "\n" )
+   staticParsed = (Gen_STATIC(GIBRL,GIBQUERY),Gen_STATIC(GIB2RL,GIB2QUERY)) #Both query's parsed and stored into a tuple.
 
-   if gib2URL == gib1URL:
+   if staticParsed[0] == staticParsed[1]:
        browser = webdriver.Firefox(executable_path=FIRE_PATH)
-       browser.get(Gen_URL(gib1URL,VALID_QUERY))
-       # print(Gen_URL(gib1URL,VALID_QUERY))
-       #ask if opened page matches the expected page.
-       # browser.find_element_by_tag_name('body').send_keys(Keys.ALT + Keys.SHIFT + 'f') # this works for some reason - only strokes sent to the body work
+       browser.get(genURL(staticParsed[1],VALID_QUERY,DEFAULT_SEP))
        pyautogui.press('f12')
        pyautogui.hotkey('ctrl','shift','c')
        #CALL WINDOW TO FEED THE TAGS
        XPATH_MODE=True
+       STATIC_URL=staticParsed[1]
        if XPATH_MODE is True:
-           elem = browser.find_element(By.XPATH,'.//*[@id="content"]')
-           manual_list = XP_to_Manual(elem.tag_name,SAMPLE_XPATH)
+           elem = browser.find_element(By.XPATH,_xpath_)
            Write_JSON(Gen_JSON(PROFILE(DEFAULT_UA,DEFAULT_REFERER,STATIC_URL,DEFAULT_SEP,manual_list[0],manual_list[1],manual_list[2])),SAMPLE_FILENAME)
        else:
            Write_JSON(Gen_JSON(PROFILE(DEFAULT_UA,DEFAULT_REFERER,STATIC_URL,DEFAULT_SEP,TAG_ID,ATTRIBUTE_ID,ATTRIBUTE_VALUE)),SAMPLE_FILENAME)
@@ -79,27 +64,56 @@ def coreSetup():
       print("REGEX_FAILURE:PARSE:1")
 #==========================
 
+#=====THE_FUNDAMENTALS=====
+def linkAnalyzer(GIBRL,GIBQUERY,GIB2RL,GIB2QUERY):
+    #
+    def Gen_STATIC(rawlink,query):
+       query_pos = rawlink.lower().find(query.lower())
+       return str(rawlink[0:query_pos]) # Cuts the string to the point until the searched query word is found.
+    #
+    staticParsed = (Gen_STATIC(GIBRL,GIBQUERY),Gen_STATIC(GIB2RL,GIB2QUERY)) #Both query's parsed and stored into a tuple.
+    if staticParsed[0] == staticParsed[1]:
+        return staticParsed[1]
+    else:
+        return None
+
+def genURL(static,query,seperator):
+   return static + query.replace(" ",seperator) #combines the static_URL with the valid search query and replaces spaces with '+'
+
+def browserInit(_url_,driverloc):
+       browser = webdriver.Firefox(executable_path=driverloc) #more browser code
+       browser.get(_url_)
+       pyautogui.press('f12')
+       pyautogui.hotkey('ctrl','shift','c')
+       return browser
+
+def profileGen():
+    STOCK_UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 RuxitSynthetic/1.0 v9139690441 t38550 ath9b965f92 altpub cvcv=2'
+    STOCK_REFERER = 'https://duckduckgo.com?q=wiki'
+    STATIC_URL=staticParsed[1]
+    #
+    def xpathGen(_driver_,_xpath_):
+        ##
+        def XP_to_Manual(tagname,xpath):
+            import re
+            TAG_LIST = [tagname]                
+            TAG_LIST += re.findall('@[a-z-]+=',xpath)
+            TAG_LIST += re.findall('\"[a-z-]+\"',xpath)
+            return [TAG_LIST[0],TAG_LIST[1][1:-1],TAG_LIST[2][1:-1]]
+        ##
+        manual_list = XP_to_Manual(_driver_.find_element(By.XPATH,_xpath_).tag_name,_xpath_)
+        Write_JSON(Gen_JSON(PROFILE(STOCK_UA,STOCK_REFERER,STATIC_URL,DEFAULT_SEP,manual_list[0],manual_list[1],manual_list[2])),SAMPLE_FILENAME)
+    #
+    def manualGen():
+        Write_JSON(Gen_JSON(PROFILE(STOCK_UA,STOCK_REFERER,STATIC_URL,DEFAULT_SEP,TAG_ID,ATTRIBUTE_ID,ATTRIBUTE_VALUE)),SAMPLE_FILENAME)
+#==========================
+
 #=====GENERATORS===========
-def Gen_STATIC(rawlink,query):
-   query_pos = rawlink.lower().find(query.lower())
-   return str(rawlink[0:query_pos]) # Cuts the string to the point until the searched query word is found.
-
-def Gen_URL(static,query):
-   return static + DEFAULT_SEP.join(str(word) for word in query) #combines the static_URL with the valid search query and replaces spaces with '+'
-
 def Gen_JSON(profileobj):
     return json.dumps(asdict(profileobj),sort_keys=True,indent=0)
 
 def Write_JSON(jsonobj,siteName):
-    open(siteName,'x').write(jsonobj)
-
-def XP_to_Manual(tagname,xpath):
-    import re
-    TAG_LIST = [tagname]                
-    TAG_LIST += re.findall('@[a-z-]+=',xpath)
-    TAG_LIST += re.findall('\"[a-z-]+\"',xpath)
-    print(TAG_LIST)
-    return [TAG_LIST[0],TAG_LIST[1][1:-1],TAG_LIST[2][1:-1]]
+    open(siteName,'w').write(jsonobj)
 #==========================
 
 coreSetup()
