edf7249c1a43fbc774b8e5262187fc2409764719
decent-man <decentboi6969@gmail.com>
2021-04-20 12:15:16 +0530

    Updated argument instructions

--- src/test.py
+++ src/test.py
@@ -1,9 +1,11 @@
 from master import infoScraper # <= import like this
-import sys                     # <= needed to read arguments
-print(infoScraper(sys.argv[1])) # <= This is only a test print. Only to show that the output of the function is the partial html file.
+#                          And search query as second input
+#                               ||
+#                               \/  
+print(infoScraper(file_path,search_query)) # <= This is only a test print. Only to show that the output of the function is the partial html file.
 #                      /\            To seperate out the text you might need to refer to the commented lines 32 & 33 in master.py - "for content in mainContent: print(content.text)" 
 #                      ||
-#           It takes file name as input
+#           It takes file name as first input
 
 # Run this file as:-  python test.py profiles/<<PROFILE_FILE>>.json <<Search_Keyword_Here>>
 #                                             /\                            /\
@@ -12,3 +14,6 @@ print(infoScraper(sys.argv[1])) # <= This is only a test print. Only to show tha
 #                                   first argument.                  second argument.
 #                                   All json files are stored
 #                                   in "profiles" directory.
+#
+#
+#     It returns the html segment as output for now. I will make it so it returns only text and title.
