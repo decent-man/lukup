e07997651cd8915cd2a64ba84d86276e1216e130
decent-man <decentboi6969@gmail.com>
2021-04-21 12:56:16 +0530

    Instructions corrected

--- src/test.py
+++ src/test.py
@@ -10,14 +10,6 @@ print(infoScraper(file_path,search_query)) # <= This is only a test print. Only 
 #                      ||
 #           It takes file name as first input
 
-# Run this file as:-  python test.py profiles/<<PROFILE_FILE>>.json <<Search_Keyword_Here>>
-#                                             /\                            /\
-#                                             ||                            ||
-#                                   Input the json file as the       Input the search term as the 
-#                                   first argument.                  second argument.
-#                                   All json files are stored
-#                                   in "profiles" directory.
-#
 #
 #     It returns the html segment as output for now. I will make it so it returns only text and title.
 
