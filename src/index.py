929b1d633839e4255833757a3db2c084f66ef806
Aniket711 <82828367+Aniket711@users.noreply.github.com>
2021-05-01 18:24:53 +0530

    Update index.py

--- src/index.py
+++ src/index.py
@@ -5,48 +5,29 @@ from tkinter import ttk
 from clip import clipcopy
 from os import listdir
 from master import infoScraper
+import tkinter.font as font
+import tkinter.scrolledtext as scrolledtext
 
 bgcolor = "#000000" #black
 fgcolor = "#F8FC07" #yellow
 
-root = Tk()                         # main screen window
-root.title('Main window')
-root.geometry("400x400+300+130")
-root.resizable(width=False, height=False)
-root.configure(bg=bgcolor)
-root.overrideredirect(True)
-b1 = Button(root, text ="Search", bg=fgcolor, command = lambda:[openwin2(), closeroot()], fg=bgcolor, font="10").place(x=30,y=80)
-b2 = Button(root, text ="+ Add website", command = lambda:[addsite(), closeroot()], bg=fgcolor, fg=bgcolor, font="10").place(x=275,y=355)
-exitbutton = Button(root, text="x", command=root.quit, bg=fgcolor, fg=bgcolor, width="2").place(x=375, y=1)
-label1 = Label(root, text= "Term to be searched is:", bg=bgcolor, fg=fgcolor, font="12").place(x=30,y=20)
-user_query = tk.StringVar(root, clipcopy())
-text = Entry(root, width="30", fg=fgcolor, bg=bgcolor, textvariable = user_query, highlightthickness=1)
-text.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-text.place(x=30,y=50)
-
-print(listdir('./profiles'))
-profilelist = tk.Listbox(root, fg=fgcolor, bg=bgcolor, highlightthickness=1)
-profilelist.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-profilelist.insert("end", *listdir('./profiles'))
 
 def getElement(event):
   selection = event.widget.curselection()
   index = selection[0]
   value = event.widget.get(index)
-  print(value)
-profilelist.bind('<<ListboxSelect>>', getElement)
-profilelist.place(x=30,y=150) 
+  profile = './profiles/' + value
+  print(profile)
+  openwin2(text.get(), profile)
 
-def closeroot():
+def openwin2(_query_, _profile_):
     root.destroy()
-
-def openwin2():
     win2 = Tk()                     # search karne pe khulne wali window
     win2.geometry("600x500+300+130")
     win2.resizable(width=False, height=False)
     win2.configure(bg=bgcolor) 
-    win2.overrideredirect(True)  
-    thecontent = infoScraper('./profiles/Wikipedia', clipcopy()) 
+    win2.overrideredirect(True)     
+    thecontent = infoScraper(_profile_, _query_) 
     label2 = Label(win2, text= thecontent[0], bg=bgcolor, fg=fgcolor).place(x=10,y=10)
     scroll = Scrollbar(win2)
     scroll.pack(side=RIGHT, fill=Y)
@@ -57,8 +38,6 @@ def openwin2():
     information.place(x=20, y=40)
     information.insert(tk.END, info)
     exitbutton = Button(win2, text="x", command=win2.quit, bg=fgcolor, fg=bgcolor, width="2").place(x=557, y=1)
-
-   
 
 def addsite():
     global win3
@@ -71,16 +50,8 @@ def addsite():
     win3.resizable(width=False, height=False)
     instructions = Text(win3, width="24",bg=bgcolor, fg=fgcolor, font="4",wrap = WORD, highlightthickness=1)
     instructions.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    instruct1 = """                  INSTRUCTIONS
-1. Search for anything gibberish on the target website -example **ASPERO0** or **DA3MON**
-2. Copy the URL of the website into the first sample box.
-3. Repeat Steps 1 & 2 for the second sample box but with a different gibberish search term.
-4. Now search for something relevant on the website and only provide the search term.
-5. lukup will open the webpage in a browser with the inspector in element selection mode.
-6. If the opened website matches the expected website proceed to the **Next** step.
-7. If not some manual intervention is required. """
     instructions.place(x=255, y=2)
-    instructions.insert(tk.END, instruct1)
+    instructions.insert(tk.END, instructor('S_PG1.md'))
     label4 = Label(win3, text="URL Sample 1", bg=bgcolor, fg=fgcolor, font="6").place(x=25,y=70)
     text4 = Entry(win3, width="35", bg=bgcolor, fg=fgcolor, highlightthickness=1)
     text4.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
@@ -98,6 +69,11 @@ def addsite():
     linkanalyse = Label(win3,text="LINK ANALYZER",bg=bgcolor, fg=fgcolor, font="15").place(x=10,y=27)
     b3 = Button(win3, text="Analyse", command = lambda:[clickanalyse(), showthis()],bg=fgcolor, fg=bgcolor, font="15").place(x=125,y=363)
 
+def instructor(_filename_):
+    docDir = './docs/'
+    with open(docDir + _filename_, 'r') as theDoc:
+        return theDoc.read()
+
 def clickanalyse():
     for widget in win3.winfo_children():
         widget.destroy()
@@ -106,16 +82,8 @@ def showthis():
     exitbutton = Button(win3, text="x", command=win3.quit, bg=fgcolor, fg=bgcolor, width="2").place(x=475, y=1)
     instructions = Text(win3, width="24",bg=bgcolor, fg=fgcolor, font="4",wrap = WORD, highlightthickness=1)
     instructions.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    instruct1 = """                  INSTRUCTIONS
-1. Search for anything gibberish on the target website -example **ASPERO0** or **DA3MON**
-2. Copy the URL of the website into the first sample box.
-3. Repeat Steps 1 & 2 for the second sample box but with a different gibberish search term.
-4. Now search for something relevant on the website and only provide the search term.
-5. lukup will open the webpage in a browser with the inspector in element selection mode.
-6. If the opened website matches the expected website proceed to the **Next** step.
-7. If not some manual intervention is required. """
     instructions.place(x=255,y=2)
-    instructions.insert(tk.END, instruct1)
+    instructions.insert(tk.END, instructor('S_PG1.md'))
     label8 = Label(win3, text="Valid Query", bg=bgcolor, fg=fgcolor, font="6").place(x=10, y=70)
     text8 = Entry(win3, width="35",  bg=bgcolor, fg=fgcolor, highlightthickness=1)
     text8.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
@@ -127,38 +95,10 @@ def clicknext():
     exitbutton = Button(win3, text="x", command=win3.quit, bg=fgcolor, fg=bgcolor, width="2").place(x=460, y=1)
     label6 = Label(win3, text="Element", bg=bgcolor, fg=fgcolor, font="10")
     label6.place(x=20, y=10)
-    scroll = Scrollbar(win3)
-    scroll.pack(side=RIGHT, fill=Y)
-    instructions = Text(win3, width="24",bg=bgcolor, fg=fgcolor, font="4",wrap = WORD, yscrollcommand = scroll.set, highlightthickness=1)
-    instructions.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    instruct2 = """           INSTRUCTIONS
-This section involves capturing accurate html tag information for lukup to scrape correctly.
-1. In the opened webpage, select and click on the element that contains the information you want to fetch.
-2. Upon clicking the element's tag will be highlighted.
-3. Beyond this there are two ways you can proceed:
-    1. **The XPATH method**:
-        This method is the "easier" option, but as all things easy it comes with a higher chance of inaccuracy.
-        - Right click on the `id` attribute of the tag and select Copy XPATH from within the menu/submenu.
-        - Paste this information in the Xpath field.
-        - Press the **Test** button.
-        - Lukup will generate appropriate profiles for the website and launch the testing sample window.
-        - If the information in the sample window is as expected then Save the configuration and exit the setup.
-    2. **The Manual Method**(Recommended):
-        This is the preferred method of grabbing element data. This method has the highest accuracy.
-        Once the required tag is highlighted in the inspector:
-        - Input the **tag** name into the **Tag** field. Usually these would be `div`,`span`,etc at the beginning right after `<`.
-        - Choose any **attribute name** in the **tag** and input this into the **Attribute Name** field. Ideally the best choice is the `id`, but in some websites there maybe more than one elements with the same id - this will cause problems. You can however enter any attribute like `class`,`itemtype`,etc just make sure it is unique.
-        - Enter the **attribute's value** in the **Attribute Value** field. This would be the value of the attribute you chose written after `=` within quotation marks.
-        - E.g For a tag like `<div id="mw-content-text" class="main-results">`
-            - `div` will go in the **Tag** field.
-            - `id` OR `class` will go in **Attribute Name** field.
-            - `mw-content-text` OR `main-results`(depending on above choice) will go in **Attribute Value** field. 
-        - Press the **Test** button.
-        - Lukup will generate appropriate profiles for the website and launch the testing sample window.
-        - If the information in the sample window is as expected then Save the configuration and exit the setup."""
-    scroll.config(command=instructions.yview, activebackground=bgcolor)
+    instructions = scrolledtext.ScrolledText(win3, width="24",bg=bgcolor, fg=fgcolor, font="4", highlightthickness=1)
+    instructions.config(highlightbackground = fgcolor, highlightcolor= fgcolor)
     instructions.place(x=240,y=2)
-    instructions.insert(tk.END, instruct2)
+    instructions.insert(tk.END, instructor('S_PG2.md'))
     cbox = ttk.Combobox(win3, values=["XPATH", "CUSTOM"], state="readonly")
     cbox.place(x=20, y=40)    
     cbox.bind("<<ComboboxSelected>>", callbackFunc)
@@ -169,11 +109,56 @@ This section involves capturing accurate html tag information for lukup to scrap
         selection = cbox.get()
     
 def callbackFunc(event):
+    label11 = Label(win3, text="Tag Name",bg=bgcolor, fg=fgcolor, font="6")
+    text11 = Entry(win3, width="30",bg = bgcolor, fg=fgcolor, highlightthickness=1)
+    text11.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+    label9 = Label(win3, text="Attribute Name",bg=bgcolor, fg=fgcolor, font="6")
+    text9 = Entry(win3, width="30",bg=bgcolor, fg=fgcolor, highlightthickness=1)
+    text9.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+   
+    label10 = Label(win3, text="Attribute Value",bg=bgcolor, fg=fgcolor, font="6")
+    text10 = Entry(win3, width="30", bg=bgcolor, fg=fgcolor, highlightthickness=1)
+    text10.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+  
+    b6 = Button(win3, text="Test",command = test, bg=fgcolor, fg=bgcolor, font="15")
+
+    label7 = Label(win3, text="Xpath",bg=bgcolor, fg=fgcolor, font="6")
+    text7 = Entry(win3, width="30", bg=bgcolor, fg=fgcolor, highlightthickness=1)
+    text7.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+  
+    b7 = Button(win3, text="Test", command=test, bg=fgcolor, fg=bgcolor, font="15")
+
+
+
     selection = event.widget.get()
-    if selection == 'CUSTOM':   
-        ifcustom()  
-    if selection == 'XPATH':
-        ifxpath()
+    if selection == 'CUSTOM': 
+        label7.destroy()
+        text7.destroy()
+        b7.destroy()
+
+        text11.place(x=20,y=90)
+        label11.place(x=20,y=70)
+        label9.place(x=50,y=130)
+        text9.place(x=20,y=150)
+        label10.place(x=20,y=180)
+        text10.place(x=20,y=210)
+        b6.place(x=150,y=335)
+  
+    elif selection == 'XPATH':
+        label7.place(x=20,y=80)
+        text7.place(x=20,y=100)
+        b7.place(x=150,y=335)
+
+        text11.destroy()
+        label11.destroy()
+        label9.destroy()
+        text9.destroy()
+        label10.destroy()
+        text10.destroy()
+        b6.destroy()
+
+    else: print(None)    
+  
     selection = ttk.Combobox(win3, width = 30)
 
 def test():
@@ -181,32 +166,26 @@ def test():
         widget.destroy()
     win3.title('info display')
 
-  
+root = Tk()                         # main screen window
+root.title('Main window')
+root.geometry("250x290+300+130")
+root.resizable(width=False, height=False)
+root.configure(bg=bgcolor)
+root.overrideredirect(True)
+b2 = Button(root, text ="+ Add website", command = lambda:[addsite(), closeroot()], bg=fgcolor, fg=bgcolor, height="1").place(x=120,y=253)
+#b2['font'] = font.Font(size=6)
+exitbutton = Button(root, text="X", command=root.quit, bg=fgcolor, fg=bgcolor, width="1").place(x=230, y=6)
+label1 = Label(root, text= "Term to be searched is:", bg=bgcolor, fg=fgcolor, font="4").place(x=30,y=20)
+user_query = tk.StringVar(root, clipcopy())
+text = Entry(root, width="30", fg=fgcolor, bg=bgcolor, textvariable = user_query, highlightthickness=1)
+text.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+text.place(x=30,y=50)
 
-def ifcustom():
-    # for widget in win3.winfo_children():
-    #     widget.destroy()
-    label11 = Label(win3, text="Tag Name",bg=bgcolor, fg=fgcolor, font="6").place(x=20,y=70)
-    text11 = Entry(win3, width="30",bg = bgcolor, fg=fgcolor, highlightthickness=1)
-    text11.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    text11.place(x=20,y=90)
-    label9 = Label(win3, text="Attribute Name",bg=bgcolor, fg=fgcolor, font="6").place(x=50,y=130)
-    text9 = Entry(win3, width="30",bg=bgcolor, fg=fgcolor, highlightthickness=1)
-    text9.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    text9.place(x=20,y=150)
-    label10 = Label(win3, text="Attribute Value",bg=bgcolor, fg=fgcolor, font="6").place(x=20,y=180)
-    text10 = Entry(win3, width="30", bg=bgcolor, fg=fgcolor, highlightthickness=1)
-    text10.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    text10.place(x=20,y=210)
-    b6 = Button(win3, text="Test",command = test, bg=fgcolor, fg=bgcolor, font="15").place(x=150,y=335)
-
-def ifxpath():
-    # for widget in win3.winfo_children():
-    #     widget.destroy()
-    label7 = Label(win3, text="Xpath",bg=bgcolor, fg=fgcolor, font="6").place(x=20,y=80)
-    text7 = Entry(win3, width="30", bg=bgcolor, fg=fgcolor, highlightthickness=1)
-    text7.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
-    text7.place(x=20,y=100)
-    b7 = Button(win3, text="Test", command=test, bg=fgcolor, fg=bgcolor, font="15").place(x=150,y=335)
+print(listdir('./profiles'))
+profilelist = tk.Listbox(root, fg=fgcolor, bg=bgcolor, highlightthickness=1, width="30")
+profilelist.configure(highlightbackground = fgcolor, highlightcolor= fgcolor)
+profilelist.insert("end", *listdir('./profiles'))
+profilelist.bind('<<ListboxSelect>>', getElement)
+profilelist.place(x=30,y=80) 
 
 mainloop()
